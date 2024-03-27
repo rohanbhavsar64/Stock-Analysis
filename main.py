1fa96b5e127b0d6863075bc565a0d702f95a5c2a
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-03-27 17:46:31 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -27,18 +27,13 @@ s=a+" "+b
 st.write(s)
 import yfinance as yf
 # Get the data for the stock AAPL
-j=['Analysis','Historical Data','Profile','Statistics']
+j=['Analysis','Profile','Statistics']
 h=st.radio('Field',j,horizontal=True)
-if h=='Historical Data':
-    l=['5d','1wk','1mo','3mo','1y']
-    i=st.selectbox('Period',l)
-    df = yf.download(ticker,period=i)
-    st.write(df)
-elif h=='Analysis':
+if h=='Analysis':
     st.subheader('Analysis')
     col1,col2=st.columns(2)
     with col1:
-        l = ['5d','1wk','1mo','3mo','1y']
+        l = ['5d','1wk','1mo','3mo','1y','5y','max']
         i = st.selectbox('Period', l)
         df = yf.download(ticker, period=i)
     with col2:
@@ -71,30 +66,6 @@ elif h=='Analysis':
             fig = px.bar(df, x=df.index, y='Close', color_discrete_sequence=["#4A7230"],title='Closing Price  vs Date')
             fig.update_yaxes(showgrid=False)
             st.write(fig)
-    url3 = f'https://finance.yahoo.com/quote/{ticker}/analysis'
-    r2 = requests.get(url3, headers=headers)
-    w2 = BeautifulSoup(r2.text, 'html')
-    a1=w2.find_all(class_='Fw(400) W(20%) Fz(xs) C($tertiaryColor) Ta(end)')[8].text
-    a2=w2.find_all(class_='Fw(400) W(20%) Fz(xs) C($tertiaryColor) Ta(end)')[9].text
-    a3=w2.find_all(class_='Fw(400) W(20%) Fz(xs) C($tertiaryColor) Ta(end)')[10].text
-    a4=w2.find_all(class_='Fw(400) W(20%) Fz(xs) C($tertiaryColor) Ta(end)')[11].text
-    b1=w2.find_all(class_='Ta(end)')[56].text
-    b2=w2.find_all(class_='Ta(end)')[57].text
-    b3=w2.find_all(class_='Ta(end)')[58].text
-    b4=w2.find_all(class_='Ta(end)')[59].text
-    c1=w2.find_all(class_='Ta(end)')[60].text
-    c2=w2.find_all(class_='Ta(end)')[61].text
-    c3=w2.find_all(class_='Ta(end)')[62].text
-    c4=w2.find_all(class_='Ta(end)')[63].text
-    data=[[a1,b1,c1],[a2,b2,c2],[a3,b3,c3],[a4,b4,c4]]
-    df17=pd.DataFrame(data,columns=['history','Est.','Actual'])
-    fig17=px.scatter(df17,x='history',y=['Est.','Actual'],title='Consensus EPS')
-    fig17.update_traces(marker=dict(size=20,
-                                  line=dict(width=2,
-                                            color='rgba(135, 206, 250, 0.5)')),
-                      selector=dict(mode='markers'))
-    st.write(fig17)
-
 
 elif h=='Profile':
     #st.subheader(ticker)
@@ -107,7 +78,7 @@ elif h=='Profile':
     u = w.find_all(class_='Pt(10px) smartphone_Pt(20px) Lh(1.7)')[0].text
     z = w.find_all(class_='D(ib) Va(t)')[0].text
     st.subheader('Description')
-    st.write(v)
+    st.write(v,fontsize=8)
 else:
     st.subheader('Statistics')
     st.markdown(
@@ -198,13 +169,40 @@ else:
     inc20 = w1.find_all(
         class_='Ta(c) Py(6px) Bxz(bb) BdB Bdc($seperatorColor) Miw(120px) Miw(100px)--pnclg Bgc($lv1BgColor) fi-row:h_Bgc($hoverBgColor) D(tbc)')[
         75].text
-    data5 = [[tr2023, inc23, '2023'], [tr2022, inc22, '2022'], [tr2021, inc21, '2021'], [tr2020, inc20, '2020']]
+    y1=w1.find_all(class_='Ta(c) Py(6px) Bxz(bb) BdB Bdc($seperatorColor) Miw(120px) Miw(100px)--pnclg D(ib) Fw(b)')[0].text
+    y2=w1.find_all(class_='Ta(c) Py(6px) Bxz(bb) BdB Bdc($seperatorColor) Miw(120px) Miw(100px)--pnclg D(ib) Fw(b) Bgc($lv1BgColor)')[0].text
+    y3=w1.find_all(class_='Ta(c) Py(6px) Bxz(bb) BdB Bdc($seperatorColor) Miw(120px) Miw(100px)--pnclg D(ib) Fw(b)')[1].text
+    y4=w1.find_all(class_='Ta(c) Py(6px) Bxz(bb) BdB Bdc($seperatorColor) Miw(120px) Miw(100px)--pnclg D(ib) Fw(b) Bgc($lv1BgColor)')[1].text
+    data5 = [[tr2023, inc23,y1], [tr2022, inc22, y2], [tr2021, inc21, y3], [tr2020, inc20, y4]]
     df21 = pd.DataFrame(data5, columns=['Total Revanue', 'income', 'Year'])
     df21['Total Revanue'] = df21['Total Revanue'].str.replace(',', '').astype(float)
     df21['income'] = df21['income'].str.replace(',', '').astype(int)
     fig21 = px.bar(df21, x='Year', y=['Total Revanue', 'income'], barmode=None,
-                   title='Revanue and Income Status Per Year')
+                   title='Revanue and Income Status Per Year',color_discrete_sequence=['blue','yellow'])
     st.write(fig21)
+    url3 = f'https://finance.yahoo.com/quote/{ticker}/analysis'
+    r2 = requests.get(url3, headers=headers)
+    w2 = BeautifulSoup(r2.text, 'html')
+    a1 = w2.find_all(class_='Fw(400) W(20%) Fz(xs) C($tertiaryColor) Ta(end)')[8].text
+    a2 = w2.find_all(class_='Fw(400) W(20%) Fz(xs) C($tertiaryColor) Ta(end)')[9].text
+    a3 = w2.find_all(class_='Fw(400) W(20%) Fz(xs) C($tertiaryColor) Ta(end)')[10].text
+    a4 = w2.find_all(class_='Fw(400) W(20%) Fz(xs) C($tertiaryColor) Ta(end)')[11].text
+    b1 = w2.find_all(class_='Ta(end)')[56].text
+    b2 = w2.find_all(class_='Ta(end)')[57].text
+    b3 = w2.find_all(class_='Ta(end)')[58].text
+    b4 = w2.find_all(class_='Ta(end)')[59].text
+    c1 = w2.find_all(class_='Ta(end)')[60].text
+    c2 = w2.find_all(class_='Ta(end)')[61].text
+    c3 = w2.find_all(class_='Ta(end)')[62].text
+    c4 = w2.find_all(class_='Ta(end)')[63].text
+    data = [[a1, b1, c1], [a2, b2, c2], [a3, b3, c3], [a4, b4, c4]]
+    df17 = pd.DataFrame(data, columns=['history', 'Est.', 'Actual'])
+    fig17 = px.scatter(df17, x='history', y=['Est.', 'Actual'], title='Consensus EPS',color_discrete_sequence=['grey','green'])
+    fig17.update_traces(marker=dict(size=20,
+                                    line=dict(width=2,
+                                              color='rgba(135, 206, 250, 0.5)')),
+                        selector=dict(mode='markers'))
+    st.write(fig17)
 
 #<div class="D(ib) W(1/2) Bxz(bb) Pend(12px) Va(t) ie-7_D(i) smartphone_D(b) smartphone_W(100%) smartphone_Pend(0px) smartphone_BdY smartphone_Bdc($seperatorColor)" data-test="left-summary-table"><table class="W(100%)"><tbody><tr class="Bxz(bb) Bdbw(1px) Bdbs(s) Bdc($seperatorColor) H(36px) "><td class="C($primaryColor) W(51%)"><span>Previous Close</span></td><td class="Ta(end) Fw(600) Lh(14px)" data-test="PREV_CLOSE-value">107.87</td></tr><tr class="Bxz(bb) Bdbw(1px) Bdbs(s) Bdc($seperatorColor) H(36px) "><td class="C($primaryColor) W(51%)"><span>Open</span></td><td class="Ta(end) Fw(600) Lh(14px)" data-test="OPEN-value">107.60</td></tr><tr class="Bxz(bb) Bdbw(1px) Bdbs(s) Bdc($seperatorColor) H(36px) "><td class="C($primaryColor) W(51%)"><span>Bid</span></td><td class="Ta(end) Fw(600) Lh(14px)" data-test="BID-value">0.00 x 900</td></tr><tr class="Bxz(bb) Bdbw(1px) Bdbs(s) Bdc($seperatorColor) H(36px) "><td class="C($primaryColor) W(51%)"><span>Ask</span></td><td class="Ta(end) Fw(600) Lh(14px)" data-test="ASK-value">106.08 x 800</td></tr><tr class="Bxz(bb) Bdbw(1px) Bdbs(s) Bdc($seperatorColor) H(36px) "><td class="C($primaryColor) W(51%)"><span>Day's Range</span></td><td class="Ta(end) Fw(600) Lh(14px)" data-test="DAYS_RANGE-value">106.75 - 108.12</td></tr><tr class="Bxz(bb) Bdbw(1px) Bdbs(s) Bdc($seperatorColor) H(36px) "><td class="C($primaryColor) W(51%)"><span>52 Week Range</span></td><td class="Ta(end) Fw(600) Lh(14px)" data-test="FIFTY_TWO_WK_RANGE-value">85.35 - 113.14</td></tr><tr class="Bxz(bb) Bdbw(1px) Bdbs(s) Bdc($seperatorColor) H(36px) "><td class="C($primaryColor) W(51%)"><span>Volume</span></td><td class="Ta(end) Fw(600) Lh(14px)" data-test="TD_VOLUME-value"><fin-streamer data-symbol="MMM" data-field="regularMarketVolume" data-trend="none" data-pricehint="2" data-dfield="longFmt" value="4,449,245" active="">4,449,245</fin-streamer></td></tr><tr class="Bxz(bb) Bdbw(1px) Bdbs(s) Bdc($seperatorColor) H(36px) Bdbw(0)! "><td class="C($primaryColor) W(51%)"><span>Avg. Volume</span></td><td class="Ta(end) Fw(600) Lh(14px)" data-test="AVERAGE_VOLUME_3MONTH-value">4,952,249</td></tr></tbody></table></div>
 ##72C73C
